e098a3556ad26a228e1112ae177463bf3a9d074b
abelgh91 <abelgh91@gmail.com>
2024-02-20 16:46:40 +0100

    contenido final

--- src/screens/AboutMe/AboutMe.jsx
+++ src/screens/AboutMe/AboutMe.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./AboutMe.css";
-import miImagen from "./Linkedin.jgp.jpg"
+import miImagen from "./Linkedin.jgp.jpg";
+import cvPdf from "./cvAbelJunior.pdf";
+import pdf from "./pdf-file-svgrepo-com.svg";
 
 const AboutMe = () => {
     return ( 
@@ -9,9 +11,17 @@ const AboutMe = () => {
             <div>
                 <h1>Sobre mí</h1>
                 <p>
-                    En los últimos meses he realizado un Bootcamp intensivo de Programación Web. Durante este periodo, he adquirido habilidades sólidas en tecnologías clave como HTML, CSS, JavaScript, Node, React, y frameworks y librerías actuales, entre otras herramientas. 
+                    En los últimos meses he realizado un Bootcamp intensivo de Programación Web. Durante este periodo, he adquirido habilidades sólidas en tecnologías clave como <strong> HTML, CSS, JavaScript, Node, React,</strong> y frameworks y librerías actuales, entre otras herramientas. 
                     Desde que finalicé este curso sigo en aprendizaje continuo desarrollando mis propios proyectos y mejorando mis habilidades, lo cual creo que es fundamental en la evolución de un desarrollador web.
                 </p>
+                <div id="cv" className="cv-container">
+                    <p><strong>Descarga mi CV</strong></p>
+                    <a href={cvPdf} download={cvPdf}>
+                        <img src={pdf} alt="cv" width="50px" height="50px" />
+                    </a>
+                </div>
+
+
             </div>
         </div>
      );
